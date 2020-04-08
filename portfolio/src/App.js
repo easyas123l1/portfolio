9b7ce123bdbe8278a36af24f87304c9016fc8674
Andrew Wilson <andrew.wilson90@hotmail.com>
2020-04-08 14:34:04 -0500

    home route

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,14 +1,15 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
+import Home from "./components/Home/Home";
 import "./styles/global.scss";
 
 function App() {
   return (
     <>
       <Switch>
-        <Route exact path="/" />
+        <Route exact path="/" component={Home} />
         {/* default route incase route doesn't exist */}
-        {/* <Route component={Home} /> */}
+        <Route component={Home} />
       </Switch>
     </>
   );
