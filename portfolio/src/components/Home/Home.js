4f51d1fe603ebfdf5584a6e41920358451ea4732
Andrew Wilson <andrew.wilson90@hotmail.com>
2021-01-27 00:52:27 -0600

    complete home page

--- portfolio/src/components/Home/Home.js
+++ portfolio/src/components/Home/Home.js
@@ -1,9 +1,23 @@
 import React from "react";
+import { NavLink as Link } from "react-router-dom";
+import portfolio from "../../styles/portfolio.module.scss";
 
 export default function Home() {
-  return (
-    <div>
-      <p>Home Portfolio</p>
-    </div>
-  );
+    return (
+        <div>
+            <div className={portfolio.image}>
+                <div className={portfolio.navbar}>
+                    <Link to="/home">Home</Link>
+                    <Link to="/aboutMe">About Me</Link>
+                    <Link to="/projects">Projects</Link>
+                </div>
+                <p className={portfolio.hello}>
+                    Hello World, I'm Andrew. A Full-Stack Web Developer.{" "}
+                </p>
+                <p className={portfolio.hello}>
+                    A Lambda School Bootcamp Graduate
+                </p>
+            </div>
+        </div>
+    );
 }
