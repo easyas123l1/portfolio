386843442acdadc6ca21467e7e19a77a39d12ac0
Andrew Wilson <andrew.wilson90@hotmail.com>
2021-01-28 01:09:07 -0600

    add routes AboutMe and Projects

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,18 +1,22 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
 import Home from "./components/Home/Home";
+import AboutMe from "./components/AboutMe/AboutMe";
+import Projects from "./components/Projects/Projects";
 import "./styles/global.scss";
 
 function App() {
-  return (
-    <>
-      <Switch>
-        <Route exact path="/" component={Home} />
-        {/* default route incase route doesn't exist */}
-        <Route component={Home} />
-      </Switch>
-    </>
-  );
+    return (
+        <>
+            <Switch>
+                <Route exact path="/" component={Home} />
+                <Route exact path="/AboutMe" component={AboutMe} />
+                <Route exact path="/Projects" component={Projects} />
+                {/* default route incase route doesn't exist */}
+                <Route component={Home} />
+            </Switch>
+        </>
+    );
 }
 
 export default App;
