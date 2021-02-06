e83410fbe331bcfe5467a493e52014f9942c66b1
Andrew Wilson <andrew.wilson90@hotmail.com>
2021-02-06 08:45:13 -0600

    fixed home link

--- portfolio/src/components/Navbar/Navbar.js
+++ portfolio/src/components/Navbar/Navbar.js
@@ -5,7 +5,7 @@ import portfolio from "../../styles/portfolio.module.scss";
 export default function Navbar() {
     return (
         <div className={portfolio.navbar}>
-            <Link to="/home">Home</Link>
+            <Link to="/">Home</Link>
             <Link to="/aboutMe">About Me</Link>
             <Link to="/projects">Projects</Link>
         </div>
