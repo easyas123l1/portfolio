250bc753b8b90179c187732cdf072dfb84a1a877
Andrew Wilson <andrew.wilson90@hotmail.com>
2021-04-11 13:47:52 -0500

    add first project link front end and back end

--- portfolio/src/components/Projects/Projects.js
+++ portfolio/src/components/Projects/Projects.js
@@ -7,7 +7,27 @@ export default function Projects() {
         <div className={portfolio.aboutMe}>
             <Navbar />
             <div>
-                <p>project 1</p>
+                <a
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    href="https://pvpwordsearchreact-git-master-andrewwilson90.vercel.app/"
+                >
+                    Word Search with PvP
+                </a>
+                <a
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    href="https://github.com/easyas123l1/pvpwordsearchreact"
+                >
+                    front end code
+                </a>
+                <a
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    href="https://github.com/easyas123l1/pvpwordsearch"
+                >
+                    back end code
+                </a>
             </div>
         </div>
     );
